a25d0d90d47e237807fa0895dc2e20b3dee6619f
Alex Dumitru <alexadrian.dmtr@gmail.com>
2016-08-18 01:51:17 +0300

    Hello World from node.js express

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var port = 3005;
+app.listen(3005, function () {
+  console.log('Example app listening on port ' + port);
 });
