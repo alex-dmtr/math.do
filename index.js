60a4eb375f91e08f517c04af48a4aa5b7c47600d
Alex Dumitru <alexadrian.dmtr@gmail.com>
2016-08-21 16:49:30 +0300

    Implement proper node server using express framework.
    html, css and javascript files moved to public/ directory.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
+var port = 3001;
+
+app.use(express.static('public'));
+
+app.use('/static', express.static(__dirname  + '/public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+// app.get('/hello', function (req, res) {
+// 	res.send("hello. is it me you're looking for?");
+// });
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 });
